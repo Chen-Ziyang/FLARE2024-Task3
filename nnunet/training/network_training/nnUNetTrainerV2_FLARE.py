1473009f96a8ba575870b1123b90a163f2289b08
Chen-Ziyang <527301770@qq.com>
2024-10-09 15:45:02 +0800

    Update

--- nnunet/training/network_training/nnUNetTrainerV2_FLARE.py
+++ nnunet/training/network_training/nnUNetTrainerV2_FLARE.py
@@ -175,6 +175,47 @@ class nnUNetTrainerV2_FLARE_Big(nnUNetTrainerUDAV2):
         self.print_to_log_file("dis_lr:", np.round(self.dis_optimizer.param_groups[0]['lr'], decimals=6))
 
 
+class nnUNetTrainerV2_FLARE_MR(nnUNetTrainerV2):
+    def initialize_network(self):
+        self.conv_per_stage = 3
+        self.base_num_features = 32
+        self.max_num_features = 512
+        self.max_num_epochs = 1000
+
+        # 取消验证，加速训练
+        self.num_val_batches_per_epoch = 1
+        self.save_best_checkpoint = False
+
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            norm_op = nn.InstanceNorm3d
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            norm_op = nn.InstanceNorm2d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        self.network = Generic_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                    len(self.net_num_pool_op_kernel_sizes),
+                                    self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
+                                    dropout_op_kwargs,
+                                    net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True,
+                                    self.max_num_features)
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = softmax_helper
+
+    def setup_DA_params(self):
+        super().setup_DA_params()
+        self.data_aug_params["do_mirror"] = False
+        self.data_aug_params["do_elastic"] = True
+
+
 class nnUNetTrainerV2_FLARE_Pseudo(nnUNetTrainerV2):
     def initialize_network(self):
         self.conv_per_stage = 3
--- utils/modify_pkl_MR-Net.py
+++ utils/modify_pkl_MR-Net.py
@@ -4,15 +4,15 @@ import pickle
 ### For MR-Net
 # Before prediction, modify DDP to normal
 root = '/data1/userdisk0/zychen/RESULTS_FOLDER/nnUNet/3d_fullres/' \
-       'Task999_PseudoMR/nnUNetTrainerV2_FLARE_Pseudo__nnUNetPlansFLARE24Big/all/model_final_checkpoint.model.pkl'
+       'Task999_PseudoMR/nnUNetTrainerV2_FLARE_MR__nnUNetPlansFLARE24Big/all/model_final_checkpoint.model.pkl'
 with open(root, 'rb') as file:
     data = pickle.load(file)
 data['init'] = ('/data1/userdisk0/zychen/RESULTS_FOLDER/nnUNet/3d_fullres/Task999_PseudoMR/'
                 'nnUNetTrainerV2__nnUNetPlansFLARE24Big/nnUNetPlansFLARE24Big_plans_3D.pkl',
                 'all', '/data1/userdisk0/zychen/RESULTS_FOLDER/nnUNet/3d_fullres/Task999_PseudoMR/'
-                       'nnUNetTrainerV2_FLARE_Pseudo__nnUNetPlansFLARE24Big',
+                       'nnUNetTrainerV2_FLARE_MR__nnUNetPlansFLARE24Big',
                 '/erwen_SSD/2T/zychen/FLARE2024-Code/nnUNet_preprocessed/Task999_PseudoMR', True, 0, True, False, False)
-data['name'] = 'nnUNetTrainerV2_FLARE_Pseudo'
+data['name'] = 'nnUNetTrainerV2_FLARE_MR'
 with open(root, 'wb') as file:
     pickle.dump(data, file)
 
